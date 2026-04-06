b1ec97617ed8a59821b8c4e6d8f0b43c56e694c3
changoloco066 <bigoteschicos@gmail.com>
2026-04-05 23:30:59 -0600

    .

--- Easy/1.- Two Sum/TwoSum.java
+++ Easy/1.- Two Sum/TwoSum.java
@@ -1,4 +1,4 @@
-class Solution {
+public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
         for(int i = 0; i < n-1; i++ ){
@@ -9,5 +9,11 @@ class Solution {
             }
         }
         return new int[]{};
+    }
+    
+    public static void main(String[] args) {
+        TwoSum solution = new TwoSum();
+        int[] result = solution.twoSum(new int[]{2,7,11,15}, 9);
+        System.out.println(result[0] + ", " + result[1]);
     }
 }
