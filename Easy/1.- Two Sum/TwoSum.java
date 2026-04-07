6b7a03394e18df07e3896eb155c34f0e57c93f61
changoloco066 <bigoteschicos@gmail.com>
2026-04-07 12:53:41 -0600

    TwoSum

--- Easy/1.- Two Sum/TwoSum.java
+++ Easy/1.- Two Sum/TwoSum.java
@@ -1,19 +1,35 @@
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        for(int i = 0; i < n-1; i++ ){
-            for(int j = i + 1; j < n; j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[]{i, j};
-                }
-            }
+        for (int i = 0; i < nums.length; i++){        // starts in the first number of the array 
+
+            for (int j = i + 1; j < nums.length; j++ ){   // starts in the second number of the array
+                if (nums[i] + nums[j] == target)
+                    return new int [] {i, j};
+            }                  
         }
-        return new int[]{};
+        return nums;
     }
     
+    // Main Method to test the solution 
     public static void main(String[] args) {
         TwoSum solution = new TwoSum();
-        int[] result = solution.twoSum(new int[]{2,7,11,15}, 9);
-        System.out.println(result[0] + ", " + result[1]);
+        
+        // Test case 1
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        int[] result1 = solution.twoSum(nums1, target1);
+        System.out.println("Test 1: [" + result1[0] + ", " + result1[1] + "]"); // Expected: [0, 1]
+        
+        // Test case 2
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        int[] result2 = solution.twoSum(nums2, target2);
+        System.out.println("Test 2: [" + result2[0] + ", " + result2[1] + "]"); // Expected: [1, 2]
+        
+        // Test case 3
+        int[] nums3 = {3, 3};
+        int target3 = 6;
+        int[] result3 = solution.twoSum(nums3, target3);
+        System.out.println("Test 3: [" + result3[0] + ", " + result3[1] + "]"); // Expected: [0, 1]
     }
 }
